20c71c4246778d269996c7885b36eca4a3cb4554
IzzAmani <izzryugasaki@gmail.com>
2026-01-13 05:33:28 +0800

    Fixed Baelz Crashing Ult

--- src/autoloads/scene_manager.gd
+++ src/autoloads/scene_manager.gd
@@ -1,14 +1,33 @@
 extends Node
 
 var scenes: Dictionary[String, Dictionary] = {
-    "Main": {
-        "scene": "",
-        "function": run_main
+    "Gameplay": {
+        "node": preload("res://src/main.tscn"),
+        "function": run_gameplay
+    },
+    "MainMenu": {
+        "node": preload("res://src/main_menu.tscn"),
+        "function": run_menu
     }
 }
 
+var current_scene: Node
+
+
+func _ready() -> void :
+    change_to_scene("Gameplay")
+
 func change_to_scene(scene_name: String) -> void :
+    var scene: PackedScene = scenes[scene_name]["node"]
+    var scene_func = scenes[scene_name]["function"]
+    
+    scene_func.call()
+    
+    
+    
+    
+func run_gameplay() -> void :
     pass
     
-func run_main() -> void :
+func run_menu() -> void :
     pass
