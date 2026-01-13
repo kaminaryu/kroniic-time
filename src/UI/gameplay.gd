5aaa84f59d53214cf26db0ce4badc7a18141dd4a
IzzAmani <izzryugasaki@gmail.com>
2026-01-14 05:35:57 +0800

    SkillCircle for each members

--- src/UI/gameplay.gd
+++ src/UI/gameplay.gd
@@ -35,18 +35,31 @@ func update_squad_circles() -> void :
     $Squad/HBoxContainer/Sana.max_value = SkillsStatisticHandler.ultimate_energy["Sana"]["Max"]
     $Squad/HBoxContainer/Sana.value = SkillsStatisticHandler.ultimate_energy["Sana"]["Current"]
     
+    
+var prev_member: String = "Hi"
 func update_skill_cooldown() -> void :
     var skill := 0
     var member: String = SquadHandler.selected_member
+    
+    if (member != prev_member) :
+        $SkillCircle/Baelz.visible = false
+        $SkillCircle/Fauna.visible = false
+        $SkillCircle/Mumei.visible = false
+        $SkillCircle/Sana.visible = false
+        
+        var skill_circle: String = "SkillCircle/" + member
+        get_node(skill_circle).visible = true
+        
+    prev_member = member
     
     # if the timer doesnt exist a.k.a havent run the skill
     # thus the skill is obv not in cooldown
     var timer: Timer = SkillsStatisticHandler.timer_running[member][skill]
     if (timer == null) :
-        $SkillCircle/TextureProgressBar.value = 0
+        $SkillCircle/Overlay.value = 0
         return
     
     var cooldown_time = SkillsStatisticHandler.skills_cooldown[member][skill]
     
-    $SkillCircle/TextureProgressBar.max_value = cooldown_time
-    $SkillCircle/TextureProgressBar.value = timer.time_left
+    $SkillCircle/Overlay.max_value = cooldown_time
+    $SkillCircle/Overlay.value = timer.time_left
